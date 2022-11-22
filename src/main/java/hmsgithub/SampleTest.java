7b38a20429a455fbf55a050829b1d7760f0a8907
dell <dell@mypc>
2022-11-22 10:30:09 -0500

    commit1

---
+++ src/main/java/hmsgithub/SampleTest.java
@@ -0,0 +1,17 @@
+package hmsgithub;
+
+
+
+public class SampleTest 
+{
+
+	   public void script1()
+	   {
+		   System.out.println("script1");  
+	   }
+	 
+	   public void script2()
+	   {
+		   System.out.println("script2");  
+	   }
+}
